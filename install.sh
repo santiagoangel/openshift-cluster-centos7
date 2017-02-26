2bc1d5f3a157f771f5a442046303726bc86b3a3c
Santiago Angel <santiagoangel@gmail.com>
2017-02-26 13:17:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,18 @@
 #start on a fresh Centos 7 install: http://isoredirect.centos.org/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-1611.iso
 #then:
 #yum install -y wget; wget https://raw.githubusercontent.com/santiagoangel/openshift-cluster-centos7/master/install.sh; sh install.sh
-
+#->answer propmt questions
 
 #install packages
 yum install -y epel-release
-yum install -y docker git ansible
+yum install -y docker git
+#yum install -y ansible
+#downgrade ansible to 2.2.0
+#yum downgrade -y ansible
+yum install -y ansible-2.2.0.0-1.el7
 yum install -y python-cryptography pyOpenSSL.x86_64
+
+
 
 #clone git projects
 cd ~
@@ -27,5 +33,6 @@ ssh-copy-id root@cloud.successfulsoftware.io
 #ansible run
 
 ansible-playbook -i ./openshift-cluster-centos7/inventory-registry-all.erb ./openshift-ansible/playbooks/byo/config.yml -vvv
+
 
 
